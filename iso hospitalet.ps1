7cb18d85ed8174c20fb8a3198e0b65906ca0bb9f
Angel-code-sys <angelignaciom77@gmail.com>
2025-02-12 11:38:25 +0100

    Add files via upload

---
+++ iso hospitalet.ps1
@@ -0,0 +1,14 @@
+﻿New-LocalGroup Hospital-Radiología
+
+foreach($usuario in Get-Content C:\Users\angel\Usur.txt)
+{
+     $usuario
+     $pass=ConvertTo-SecureString "1188200ddww" -AsPlainText -Force
+     New-LocalUser $usuario -Password $pass 
+}
+
+Add-LocalGroupMember -Group Hospital-Radiología -Member Juan
+
+Get-LocalGroup
+Get-LocalGroupMember
+
